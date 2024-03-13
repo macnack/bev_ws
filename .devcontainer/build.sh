b5844e5286a57eb0eb6e2ca27c4aebfac710b260
Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>
2024-03-13 15:20:56 +0100

    init proj
    Signed-off-by: Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,3 +1,3 @@
 DOCKER_BUILDKIT=1 docker build --network=host \
-    --build-arg WORKSPACE=ros2_ws \
-    -t amadeuszsz/ros2_project:humble .
+    --build-arg WORKSPACE=bev_ws \
+    -t macnack/bev_project:humble .
--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
-rocker --network host --privileged --nvidia --x11 --user --name ros_container \
+rocker --network host --privileged --nvidia --x11 --user --name bev \
     --env "USER" \
     --env "RMW_IMPLEMENTATION=rmw_fastrtps_cpp" \
     --env "ROS_DOMAIN_ID=0" \
     --volume "${PWD}:${HOME}/${PWD##*/}" \
+    --volume "${PWD}/../bev_data:${HOME}/${PWD##*/}/../bev_data" \
+    --volume /tmp/.X11-unix:/tmp/.X11-unix \
     --volume /dev/:/dev/ \
-    -- amadeuszsz/ros2_project:humble 
+    -- macnack/bev_project:humble 
